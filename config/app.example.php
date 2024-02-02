df47b757515257544ae1003ef3151473167c2875
echtyushi <ja.agripo2003@gmail.com>
2024-02-02 16:18:32 +0100

    Added configuration implementation for ease-of-use when retrieving config values

---
+++ config/app.example.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Configuration
+|--------------------------------------------------------------------------
+|
+| This file contains configuration settings for your application.
+| including the port and any other relevant components, such as routes, ports
+| or subdomains, necessary for accessing the entrypoint of your application.
+|
+|--------------------------------------------------------------------------
+*/
+
+return [
+
+    /**
+     * Make sure that the token contains the necessary permissions for
+     * this application to function properly
+     */
+    'api_token' => '',
+
+    /**
+     * Ensure that the client has admin permissions over
+     * every account and zone to avoid conflicts
+     */
+    'api_client_id' => '',
+
+    /**
+     * URL to the api
+     */
+    'api_url' => 'https://api.cloudflare.com/client/v4',
+
+];
